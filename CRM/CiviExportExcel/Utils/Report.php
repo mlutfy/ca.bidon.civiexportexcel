c8fb67bf05d064afe02086aba2f422a6bb72ca9f
Mathieu Lutfy <mathieu@bidon.ca>
2015-08-31 09:19:20 -0400

    Increase the number of columns up to 'Mx'.. some people really have huge reports

--- CRM/CiviExportExcel/Utils/Report.php
+++ CRM/CiviExportExcel/Utils/Report.php
@@ -44,7 +44,7 @@ class CRM_CiviExportExcel_Utils_Report {
     $csv = '';
 
     // Generate an array with { 0=>A, 1=>B, 2=>C, ... }
-    $foo = array(0 => '', 1 => 'A', 2 => 'B', 3 => 'C', 4 => 'D', 5 => 'E');
+    $foo = array(0 => '', 1 => 'A', 2 => 'B', 3 => 'C', 4 => 'D', 5 => 'E', 6 => 'F', 7 => 'G', 8 => 'H', 9 => 'I', 10 => 'J', 11 => 'K', 12 => 'L', 13 => 'M');
     $a = ord('A');
     $cells = array();
 
